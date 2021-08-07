939bf3844d5e4253f531f296d7fb9cf543297b2f
Maria Solano <majosolano99@gmail.com>
2021-08-07 19:21:31 -0400

    Use updated schema

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
 import { GetStaticProps } from 'next';
 
-// import getGraphQLClient from 'graphQLClient';
-// import { getSdk } from 'graphql-server/sdk';
+import getGraphQLClient from 'graphQLClient';
+import { getSdk } from 'graphql-server/sdk';
 import useLanguageContext from 'context/languageContext';
 import { getPlaceholder } from 'utils/cloudinary';
-import { CLOUDINARY_BASE_URI, 
-    // FEATURED_EXPERIENCES_IDS 
-} from 'global-constants';
+import { CLOUDINARY_BASE_URI, FEATURED_EXPERIENCES_IDS } from 'global-constants';
 import type { Image } from 'models/files';
-// import type { CardContentFragment as ExperienceCard } from 'graphql-server/sdk';
+import type { CardContentFragment as ExperienceCard } from 'graphql-server/sdk';
 
 import RambleHead from 'components/RambleHead';
 import ResetPasswordDialog from 'components/ResetPasswordDialog';
 import Collage from 'components/home-page/Collage';
-// import FeaturedExperiences from 'components/home-page/FeaturedExperiences';
+import FeaturedExperiences from 'components/home-page/FeaturedExperiences';
 import GallerySlide from 'components/home-page/GallerySlide';
 import Footer from 'components/Footer';
 
@@ -21,7 +19,7 @@ type Props = {
     collageImages: Image[];
     partakeImages: Image[];
     adventureImages: Image[];
-    // featuredExperiences: ExperienceCard[];
+    featuredExperiences: ExperienceCard[];
 }
 
 const GRID_IMGS_URLS = [
@@ -44,8 +42,8 @@ const ADVENTURE_IMGS_URLS = [
     'v1628286916/Ramble/Homepage/adventure3.jpg'
 ] as const;
 
-// const graphQLClient = getGraphQLClient();
-// const sdk = getSdk(graphQLClient);
+const graphQLClient = getGraphQLClient();
+const sdk = getSdk(graphQLClient);
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
     // Images for the landing collage
@@ -72,17 +70,17 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
     const adventureImages = await Promise.all(adventureImagesPromises);
 
     // Featured experiences
-    // const experiencesData = await sdk.getFeaturedExperiences({
-    //     ids: FEATURED_EXPERIENCES_IDS
-    // });
-    // const featuredExperiences = experiencesData.experiencesById;
+    const experiencesData = await sdk.getFeaturedExperiences({
+        ids: FEATURED_EXPERIENCES_IDS
+    });
+    const featuredExperiences = experiencesData.experiencesById;
 
     return {
         props: {
             collageImages,
             partakeImages,
             adventureImages,
-            // featuredExperiences
+            featuredExperiences
         }
     }
 }
@@ -98,7 +96,7 @@ const Home = (props: Props) => {
             imageUrl={`${process.env.RAMBLE_URL}/public/images/ramble-brand.png`} />
             <ResetPasswordDialog />
             <Collage images={props.collageImages} />
-            {/* <FeaturedExperiences experiences={props.featuredExperiences} /> */}
+            <FeaturedExperiences experiences={props.featuredExperiences} />
             <GallerySlide 
             images={props.partakeImages}
             title={text.partakeTitle}
